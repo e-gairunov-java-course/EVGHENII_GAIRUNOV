8b6c28897331b87ca5d5871d80365717727567dc
Evghenii <syrius@inbox.ru>
2024-12-22 19:08:25 +0200

    HW18

---
+++ src/main/java/homework_nr_18/WrongFormatException.java
@@ -0,0 +1,8 @@
+package homework_nr_18;
+
+public class WrongFormatException extends Exception {
+  public WrongFormatException(String message) {
+        super(message);
+    }
+  public WrongFormatException() { super(); }
+}
